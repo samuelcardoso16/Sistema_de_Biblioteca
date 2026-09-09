1173557eab88ae6677c6e499a3b043a6541b2fa0
alansilvadeoliveira169-max <alansilvadeoliveira169@gmail.com>
2026-09-09 08:23:01 -0300

    Update Samuel_Class-biblioteca.py

--- Samuel_Class-biblioteca.py
+++ Samuel_Class-biblioteca.py
@@ -2,11 +2,21 @@ class Biblioteca:
     def __init__(self):
         self.leitores= []
         self.livros=[]
+        
     def cadastrar_leitor(self,leitor):
         self.leitores.append(leitor)
+
     def cadastrar_livro(self,livro):
         self.livros.append(livro)
+
     def buscar_leitor(self,nome):
         for leitor in self.leitores: 
             if leitor.nome.lower()== nome.lower():
                 return leitor
+            
+    def buscar_livro(self,titulo):
+        for livro in self.livros:
+            if livro.titulo.lower()== titulo.lower():
+                return livro
+            return None
+            
