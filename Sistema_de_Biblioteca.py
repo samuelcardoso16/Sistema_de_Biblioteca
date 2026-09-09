8f3098c6c8260307c136015834228bba5891679a
alansilvadeoliveira169-max <alansilvadeoliveira169@gmail.com>
2026-09-09 09:36:48 -0300

    Update Sistema_de_Biblioteca.py

--- Sistema_de_Biblioteca.py
+++ Sistema_de_Biblioteca.py
@@ -1,19 +1,1 @@
-class Administrador(Pessoa):
-    def __init__(
-        self,
-        nome,
-        idade,
-        telefone,
-        email,
-        matricula
-    ):
-        super().__init__(
-            nome,
-            idade,
-            telefone,
-            email
-        )
-        self.matricula = matricula
 
-# Cadastro do administrador
-administrador = Administrador("Alan Silva De Oliveira",30,"(77) 99999-0000","alansilvadeoliveira169@gmail.com","ADM123")
